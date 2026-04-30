3acf4923b1c32c3c68b805992eea8ba739bd9777
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2026-04-30 17:53:47 +0000

    add

--- python_uni.py
+++ python_uni.py
@@ -1,2 +1,33 @@
 hello = "hello world"
 print(hello)
+
+n1=7
+print(n1)
+n2=3
+print(n2)
+c = n1 + n2
+print(c)
+
+n1 = 99
+print(n1)
+
+myname = "gio"
+print("my name is " + myname)
+
+
+##########
+
+myname= "gio"
+print("welcome to " + myname)
+
+my_name = "ren"
+print("welcome to",  myname + " "  + my_name + " age of", 26)
+
+age = 1
+
+for x in range(10):
+     print("my_name")
+
+for y in range(20):
+    print(age)
+    age = age + 1
